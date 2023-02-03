4fe2d56c64b0cacbe6b0a77bf5e7b0fc88f0f786
Sorokovsky <Sorokovskys@ukr.net>
2023-02-03 19:59:26 +0300

    fastif

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 const PORT:number = parseInt(process.env.PORT) || 5600; 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   await app.listen(PORT, () => console.log(`Server has been started on ${PORT}`));
 }
 bootstrap();
